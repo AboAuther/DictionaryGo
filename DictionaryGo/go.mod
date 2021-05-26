04966a7487e81736bc591c1a072a5a3af57100be
AboAuther <1256446353@qq.com>
2021-05-26 22:32:09 +0800

    DictionaryGo

---
+++ DictionaryGo/go.mod
@@ -0,0 +1,8 @@
+module DictionaryGo
+
+go 1.16
+
+require (
+	github.com/satori/go.uuid v1.2.0
+	github.com/spf13/cobra v1.1.3
+)
--- DictionaryGo/main.go
+++ DictionaryGo/main.go
@@ -4,6 +4,7 @@ import (
 	"crypto/sha256"
 	"encoding/json"
 	"fmt"
+	uuid "github.com/satori/go.uuid"
 	"io"
 	"io/ioutil"
 	"log"
@@ -15,7 +16,7 @@ import (
 	"strings"
 	"time"
 
-	uuid "github.com/satori/go.uuid"
+	_ "github.com/satori/go.uuid"
 	"github.com/spf13/cobra"
 )
 
@@ -58,33 +59,30 @@ type Config struct {
 	AppSecret string `json:"AppSecret"`
 }
 
-var fromLang string
-var toLang string
-var config Config
-
 func main() {
-
-	var command = &cobra.Command{Use: "app {word}", Short: "translate words",
+	var fromLang string
+	var toLang string
+	var command = &cobra.Command{Use: "DictionaryGo [word]", Short: "translate words",
 		Long: `translate words to other language by cmdline`,
 		Args: cobra.MinimumNArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			curFilePath := GetFilePath()
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
 			words := strings.Join(args, " ")
-			err := InitConfig(curFilePath+"/config.json", &config)
-			if err != nil {
-				fmt.Println("config file open fail")
-				return
-			}
-			httpPost(words, fromLang, toLang)
+			config := InitConfig()
+			jsonContent := getJson(words, fromLang, toLang, config)
+			PrintTranslation(&jsonContent, os.Stdout)
+			return
 		}}
 	command.Flags().StringVarP(&fromLang, "from", "f", "auto", "translate from this language")
 	command.Flags().StringVarP(&toLang, "to", "t", "auto", "translate to this language")
-	_ = command.Execute()
+	err := command.Execute()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-//httpPost 调用API，查询翻译结果
-func httpPost(words, fromLang, toLang string) {
-	uuidRandNum, _ := uuid.NewV4()
+//getJson 调用API，查询翻译结果
+func getJson(words, fromLang, toLang string, config *Config) (jsonContent DictionaryRespJson) {
+	uuidRandNum := uuid.NewV4()
 
 	input := TruncateString(words)
 	stamp := time.Now().Unix()
@@ -103,7 +101,7 @@ func httpPost(words, fromLang, toLang string) {
 	data["curtime"] = []string{strconv.FormatInt(stamp, 10)}
 
 	resp, err := http.PostForm("https://openapi.youdao.com/api", data)
-	if err != nil {
+	if err != nil || resp == nil {
 		fmt.Printf("open api failed,%v\n", err)
 	}
 	defer resp.Body.Close()
@@ -111,71 +109,89 @@ func httpPost(words, fromLang, toLang string) {
 	if err != nil {
 		fmt.Printf("translate the word failed,%v\n", err)
 	}
-	var jsonContent DictionaryRespJson
-	_ = json.Unmarshal(body, &jsonContent)
-	PrintTranslation(&jsonContent, os.Stdout)
+	err1 := json.Unmarshal(body, &jsonContent)
+	if err1 != nil {
+		fmt.Println("config unmarshal failed")
+	}
+	return jsonContent
 }
 
 //PrintTranslation 将报文内容结构化显示
 func PrintTranslation(jsonContent *DictionaryRespJson, writer io.Writer) {
 	if jsonContent.ErrorCode != "0" {
-		_, _ = fmt.Fprintln(writer, "Please input right data")
-	}
-	_, _ = fmt.Fprintln(writer, "----", jsonContent.Query, "----")
-	if jsonContent.Basic.UkPhonetic != "" {
-		_, _ = fmt.Fprintln(writer, "英式发音:", "[ ", jsonContent.Basic.UkPhonetic, " ]")
-	}
-	if jsonContent.Basic.UsPhonetic != "" {
-		_, _ = fmt.Fprintln(writer, "美式发音:", "[ ", jsonContent.Basic.UsPhonetic, " ]")
-	}
-	_, _ = fmt.Fprintln(writer, "[ 翻译结果 ]")
-	for k, v := range jsonContent.Translation {
-		_, _ = fmt.Fprintln(writer, "\t", k+1, ".", v)
-	}
-	if jsonContent.Basic.Explains != nil {
-		_, _ = fmt.Fprintln(writer, "[ 网络释义 ]")
-		for k, v := range jsonContent.Basic.Explains {
+		switch jsonContent.ErrorCode {
+		case "102":
+			_, _ = fmt.Fprintln(writer, "Language type not supported")
+		case "103":
+			_, _ = fmt.Fprintln(writer, "Translation text too long")
+		case "108":
+			_, _ = fmt.Fprintln(writer, "Invalid AppID,please confirm you ID")
+		case "113":
+			_, _ = fmt.Fprintln(writer, "The text to be queried is empty")
+		case "401":
+			_, _ = fmt.Fprintln(writer, "The account is overdue. Please recharge the account")
+		default:
+			_, _ = fmt.Fprintln(writer, "Please input right data")
+		}
+	} else {
+		_, _ = fmt.Fprintln(writer, "----", jsonContent.Query, "----")
+		if jsonContent.Basic.UkPhonetic != "" {
+			_, _ = fmt.Fprintln(writer, "英式发音:", "[ ", jsonContent.Basic.UkPhonetic, " ]")
+		}
+		if jsonContent.Basic.UsPhonetic != "" {
+			_, _ = fmt.Fprintln(writer, "美式发音:", "[ ", jsonContent.Basic.UsPhonetic, " ]")
+		}
+		_, _ = fmt.Fprintln(writer, "[ 翻译结果 ]")
+		for k, v := range jsonContent.Translation {
 			_, _ = fmt.Fprintln(writer, "\t", k+1, ".", v)
 		}
-	}
-	if jsonContent.Web != nil {
-		_, _ = fmt.Fprintln(writer, "[ 延伸释义 ]")
-		for k, v := range jsonContent.Web {
-			_, _ = fmt.Fprintln(writer, "\t", k+1, ".", v.Key)
-			_, _ = fmt.Fprint(writer, "\t翻译:")
-			for _, val := range v.Value {
-				_, _ = fmt.Fprint(writer, val, ",")
+		if jsonContent.Basic.Explains != nil {
+			_, _ = fmt.Fprintln(writer, "[ 网络释义 ]")
+			for k, v := range jsonContent.Basic.Explains {
+				_, _ = fmt.Fprintln(writer, "\t", k+1, ".", v)
 			}
-			_, _ = fmt.Fprint(writer, "\n")
+		}
+		if jsonContent.Web != nil {
+			_, _ = fmt.Fprintln(writer, "[ 延伸释义 ]")
+			for k, v := range jsonContent.Web {
+				_, _ = fmt.Fprintln(writer, "\t", k+1, ".", v.Key)
+				_, _ = fmt.Fprint(writer, "\t翻译:")
+				for _, val := range v.Value {
+					_, _ = fmt.Fprint(writer, val, ",")
+				}
+				_, _ = fmt.Fprint(writer, "\n")
+			}
+		}
+		if jsonContent.WebDict != nil {
+			_, _ = fmt.Fprintln(writer, "[ 查看详情:]")
+			for _, v := range jsonContent.WebDict {
+				_, _ = fmt.Fprintln(writer, "\t", v)
+			}
 		}
 	}
-	if jsonContent.WebDict != nil {
-		_, _ = fmt.Fprintln(writer, "[ 查看详情:]")
-		for _, v := range jsonContent.WebDict {
-			_, _ = fmt.Fprintln(writer, "\t", v)
-		}
-	}
+
 }
 
 //InitConfig 加载配置文件
-func InitConfig(cfgPath string, cfgFile *Config) error {
-	fileObj, err := os.Open(cfgPath)
+func InitConfig() *Config {
+	var config Config
+	curFilePath := GetFilePath()
+	content, err := ioutil.ReadFile(curFilePath + "/config.json")
 	if err != nil {
-		return err
+		fmt.Println("config file open failed")
 	}
-	defer fileObj.Close()
-	var fileContext []byte
-	fileContext, err = ioutil.ReadAll(fileObj)
-	_ = json.Unmarshal(fileContext, cfgFile)
-	return err
+	err1 := json.Unmarshal(content, &config)
+	if err1 != nil {
+		fmt.Println("config unmarshal failed")
+	}
+	return &config
 }
 
 //GetFilePath 获取当前文件地址
 func GetFilePath() string {
 	path, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	fmt.Println(path)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("get file's path failed")
 		return ""
 	}
 	newPath := strings.Replace(path, "\\", "/", -1)
@@ -185,15 +201,15 @@ func GetFilePath() string {
 //TruncateString 截断查询字符串
 func TruncateString(q string) string {
 	res := make([]rune, 10)
-	temp := []rune(q)
+	temp := []rune(q) //将字符串转为Int32(以防中文)
 	qLen := len(temp)
 	if qLen <= 20 {
 		return q
 	} else {
-		res = temp[:10]
-		strQLen := strconv.Itoa(qLen) //字符串q长度转为字符串
-		res = append(res, []rune(strQLen)...)
-		res = append(res, temp[qLen-10:qLen]...)
+		res = temp[:10]                          //赋值给返回结果
+		strQLen := strconv.Itoa(qLen)            //将字符串q长度转为字符串
+		res = append(res, []rune(strQLen)...)    //将字符串转为rune加入返回结果
+		res = append(res, temp[qLen-10:qLen]...) //再取后10位
 		return string(res)
 	}
 }
